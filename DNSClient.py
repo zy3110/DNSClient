a904b44911603d899992eb6c5fe6a62a5da94ec5
yang5313876111 <158246318+yang5313876111@users.noreply.github.com>
2024-02-07 17:38:35 +0800

    Update DNSClient.py

--- DNSClient.py
+++ DNSClient.py
@@ -1,5 +1,5 @@
 import dns.resolver
-
+   
 # Set the IP address of the local DNS server and a public DNS server
 local_host_ip = '208.67.222.222'
 real_name_server = '8.8.8.8' # Research public DNS servers to find a valid DNS server IP address to use
